f986b9bc2c715570c18dbb2fc8b1f37e9a80b992
johnobong <jonbong98@gmail.com>
2026-01-03 17:43:58 +0000

    arguments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_book_text,
     count_words,
@@ -8,14 +10,21 @@ from stats import (
 
 
 def main():
-    words = get_book_text("books/frankenstein.txt")
+    # book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+
+    words = get_book_text(book_path)
     word_list = get_word_list(words)
     num_words = count_words(word_list)
     num_letters = count_characters(word_list)
     formatted_count = format_dict(num_letters)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
