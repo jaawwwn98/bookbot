8f6f354a5fa0458d64d55b9f3e5ee356b6b39a99
johnobong <jonbong98@gmail.com>
2026-01-03 16:40:33 +0000

    get book text and count words

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as file:
+        file_contents = file.read()
+
+    return file_contents
+
+
+def count_words(words: str) -> int:
+    word_list = words.split()
+    return len(word_list)
+
+
+def main():
+    words = get_book_text("books/frankenstein.txt")
+    num_words = count_words(words)
+    print(f"Found {num_words} total words")
+
+
+main()
