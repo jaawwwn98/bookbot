3419e892b361ae9c4f3337713c527b17a4738388
johnobong <jonbong98@gmail.com>
2026-01-03 17:16:19 +0000

    count characters

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from stats import get_book_text, count_words
+from stats import get_book_text, count_words, get_word_list, count_characters
 
 
 def main():
     words = get_book_text("books/frankenstein.txt")
-    num_words = count_words(words)
+    word_list = get_word_list(words)
+    num_words = count_words(word_list)
+    num_letters = count_characters(word_list)
     print(f"Found {num_words} total words")
+    print(num_letters)
 
 
 main()
--- stats.py
+++ stats.py
@@ -5,6 +5,24 @@ def get_book_text(filepath: str) -> str:
     return file_contents
 
 
-def count_words(words: str) -> int:
-    word_list = words.split()
+def get_word_list(words: str) -> list[str]:
+    return words.lower().split()
+
+
+def count_words(word_list: list) -> int:
     return len(word_list)
+
+
+def count_characters(word_list: list) -> dict[str, int]:
+    character_counts = {}
+
+    for word in word_list:
+        for letter in word:
+            if not letter.isalpha():
+                continue
+            elif letter not in character_counts:
+                character_counts[letter] = 1
+            else:
+                character_counts[letter] += 1
+
+    return character_counts
